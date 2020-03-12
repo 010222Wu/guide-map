cb94ede1a94edf19fcdf936c071e8af83b952d5a
zephyr19 <a550196504@gmail.com>
2020-03-12 22:52:13 +0800

    finish hw

--- pages/test/test.js
+++ pages/test/test.js
@@ -3,6 +3,10 @@
 //调用list页面所需信息
 var listData = require('../../data/list.js');
 
+// toasjdfk
+var toView;
+var widths;
+
 Page({
 
   /**
@@ -14,24 +18,28 @@ Page({
     statusBarHeight: 0,
     titleBarHeight: 0,
     mapHeight: 0,
-    // 
     menu: [],
     listItem: null,
+    scale: 18,
     // 这里的lon、lat默认为中南大学南校区文法楼
     longitude: 112.936395,
     latitude: 28.160311,
     // 弹窗
     show: true,
+    toView: '',
+    srollLeft: 0,
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    let name = listData.init[0].varName
     this.getWindowHeight();
     this.setData({
       menu: listData.init,
-      listItem: listData.menzhen[0].content
+      listItem: listData[name][0].content,
+      toView: listData[name][0].content[0].id,
     })
   },
 
@@ -39,7 +47,15 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    // 获取menu-item宽度信息
+    const query = wx.createSelectorQuery()
+    query.selectAll('.menu-item').boundingClientRect()
+    query.exec(function (res) {
+      widths = new Array(res[0].length)
+      for (let i = 0; i < widths.length; i++) {
+        widths[i] = res[0][i].left
+      }
+    })
   },
 
   /**
@@ -115,9 +131,48 @@ Page({
     })
   },
 
+  /**
+   * 更新列表
+   */
+  updateList: function (e) {
+    let index = e.currentTarget.dataset.index
+    let name = this.data.init[index].varName
+    this.setData({
+      listItem: listData[name][0].content,
+      toView: listData[name][0].content[0].id,
+      scrollLeft: widths[index - 1],
+      show: true,
+    })
+  },
+
   onClose: function () {
     this.setData({
       show: false
+    })
+  },
+
+  tapMarker: function (e) {
+    toView = e.markerId
+    this.setData({
+      show: true,
+    })
+  },
+
+  scrollIntoView: function () {
+    this.setData({
+      toView: toView,
+    })
+  },
+
+  selectDestination: function (e) {
+    let index = e.currentTarget.dataset.index
+    let item = this.data.listItem[index]
+    this.setData({
+      longitude: item.longitude,
+      latitude: item.latitude,
+      toView: item.id,
+      scale: 18,
+      show: false,
     })
   }
 })
